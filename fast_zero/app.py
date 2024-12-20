c6afc8ef83afdec5aeb06890122c9107d4addfea
Patrick Covre Rodrigues <patrickcovredev@gmail.com>
2024-12-20 13:44:55 -0300

    alter localhost 3k corsorigin

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -20,16 +20,16 @@ from .routers import todos, activity, project
 app = FastAPI()
 
 origins = [
+    "http://localhost",
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],     
-    allow_headers=["*"],     
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(todos.router, prefix="/todos", tags=["todos"])
